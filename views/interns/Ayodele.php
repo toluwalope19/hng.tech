33366eea1b61acd092c8e9a33d58a4bba1e61076
Ayodele Tolulope <ayodeletolulope18@gmail.com>
2019-04-04 10:37:59 +0100

    added Ayodele file

---
+++ views/interns/Ayodele.php
@@ -0,0 +1,39 @@
+<head><link rel="stylesheet" href="app/css/app.css?version=1'" type="text/css"/></head>
+
+<html>
+   <?php include 'partials/header.php';?>
+   <body class=" pi">
+      <?php include 'partials/navbar.php';?>
+      <section class="main-section">
+         <div class="container">
+            <div class="row">
+               <div class="col-md-12 col-sm-6 col-xs-12">
+                  <div class="col-md-6 col-xs-12">
+                     <div class="intern-block border-blue">
+                        <img src="https://res.cloudinary.com/hng-intern1/image/upload/v1554309867/Ayodele_2.jpg" alt="Ayodele Tolulope">
+                        <h6>Ayodele Tolulope</h6>
+                        <p>Front-end Engineer/ UI/UX Designer</p>
+                        <ul class="list">
+                           <li><a href="https://twitter.com/tolubarca" target="_blank"<i class="fab fa-twitter fa-2x"></i></a></li>
+                           <li><a href="https://github.com/toluwalope19" target="_blank"<i class="fab fa-github fa-2x"></i></a></li>
+                           <li><a href="https://medium.com/@ayodeletolulope18" target="_blank"<i class="fab fa-medium fa-2x"></i></a></li>
+                        </ul>
+                     </div>
+                  </div>
+                  <div class="col-md-6 col-xs-12 align-items-center">
+                     <div class="intern-bio border-blue">
+                        <h6>About me</h6>
+                        <p>I'm a GIS Technician currently undergoing the HNG internship which will help me reach my goal of becoming a software developer someday. I want to create software solutions to everyday geospatial issues.</p>
+                        <br />
+                        <h6>Skills</h6>
+                        <p>JavaScript | Figma | CSS</p>
+                     </div>
+                  </div>
+               </div>
+            </div>
+         </div>
+         </div>
+      </section>
+   </body>
+   <?php include 'partials/footer.php';?>
+</html>
